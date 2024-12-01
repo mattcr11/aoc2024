1584d9eb257b32865cbe215d354a20d5d56f2f0e
Matthew Rucker <mattcr11@gmail.com>
2024-12-01 15:43:06 +0000

    Day 01 Part 01 Solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,37 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    // convert string into vector of vectors of numbers in the same configuration
+    let pairs = input
+        .lines()
+        .map(|line| {
+            line.split("   ")
+                .map(|item| item.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    // split the lists into individual vectors and sort
+    let sorted = (0..pairs[0].len())
+        .map(|list_index| {
+            let mut unsorted = pairs
+                .iter()
+                .map(|line| line[list_index])
+                .collect::<Vec<_>>();
+            unsorted.sort();
+            unsorted
+        })
+        .collect::<Vec<_>>();
+
+    // compute pair-wise distances
+    let distances = (0..pairs.len()).map(|line_index| {
+        (0..sorted.len()).map(|list_index| {
+            sorted[list_index][line_index]
+        }).reduce(|acc, e| acc.abs_diff(e)).unwrap()
+    }).collect::<Vec<_>>();
+
+    // sum the distances
+    Some(distances.iter().sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +45,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
