b826f60fb7878e489532c49f4428fd0758ccb853
Matthew Rucker <mattcr11@gmail.com>
2024-12-06 03:24:50 +0000

    Day 02 Part 02 Solution

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,3 @@
-use std::{cmp::Ordering, iter::repeat};
-
 use itertools::Itertools;
 
 advent_of_code::solution!(2);
@@ -9,7 +7,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    solve_part_two(input).ok()
 }
 
 fn solve_part_one(input: &str) -> anyhow::Result<u32> {
@@ -17,6 +15,14 @@ fn solve_part_one(input: &str) -> anyhow::Result<u32> {
     Ok(reports
         .iter()
         .filter_map(|item| is_report_safe_part1(item).then_some(()))
+        .count() as u32)
+}
+
+fn solve_part_two(input: &str) -> anyhow::Result<u32> {
+    let reports = parse_input(input)?;
+    Ok(reports
+        .iter()
+        .filter_map(|item| is_report_safe_part2(item).then_some(()))
         .count() as u32)
 }
 
@@ -29,27 +35,37 @@ fn analyze_report_differences(report: &Vec<i32>) -> Vec<i32> {
 }
 
 fn find_first_fault(analysis: &Vec<i32>) -> Option<usize> {
-    let first_non_zero = analysis
-        .iter()
-        .find(|item| item.cmp(&&0) != Ordering::Equal);
+    let first_non_zero = analysis.iter().find(|item| **item != 0);
     match first_non_zero {
         Some(first_non_zero) => {
             let starting_sign = first_non_zero.signum();
             analysis
                 .iter()
                 .find_position(|item| {
-                    item.cmp(&&0) == Ordering::Equal
-                        || item.signum() != starting_sign
-                        || item.abs() > 3
+                    **item == 0 || item.signum() != starting_sign || item.abs() > 3
                 })
                 .map(|(pos, _)| pos)
         }
-        None => None,
+        None => Some(0),
     }
 }
 
 fn is_report_safe_part1(report: &Vec<i32>) -> bool {
     find_first_fault(&analyze_report_differences(report)).is_none()
+}
+
+fn is_report_safe_part2(report: &Vec<i32>) -> bool {
+    match find_first_fault(&analyze_report_differences(report)) {
+        Some(fault_position) => (fault_position.saturating_sub(1)
+            ..report.len().min(fault_position + 2)) // check positions on both sides of fault
+            .map(|report_position| {
+                let mut tolerated_report = report.clone();
+                let _ = tolerated_report.remove(report_position as usize);
+                is_report_safe_part1(&tolerated_report)
+            })
+            .any(|safe| safe),
+        None => true,
+    }
 }
 
 fn parse_input(input: &str) -> anyhow::Result<Vec<Vec<i32>>> {
@@ -76,6 +92,13 @@ mod tests {
 
         let unsafe_result = is_report_safe_part1(&reports[1]);
         assert!(!unsafe_result);
+    }
+
+    #[test]
+    fn test_report_safe_part2() {
+        let report = vec![31, 32, 30, 33, 34, 37];
+        let result = is_report_safe_part2(&report);
+        assert!(result);
     }
 
     #[test]
@@ -110,6 +133,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
